9fc70fe89a61ed43247dd210a7ba696a1b478e0b
Zesty-Zen <ibrahimmamoon7@gmail.com>
2024-03-31 09:13:05 +0500

    Adding welcome_email feature whenever a new user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,19 @@ class User < ApplicationRecord
   has_many :quotes
 
   def name
-    email.split("@").first.capitalize
+    #email.split("@").first.capitalize       #Splits at first occurence of @
+    email.split(/\d/, 2).first.capitalize    #Splits at first occurence of any number
+
+  end
+
+  
+
+  after_create :send_welcome_email
+
+  private
+
+  def send_welcome_email
+    UserMailer.with(user: self).welcome_email.deliver_now
   end
 
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  
+
+
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -73,4 +78,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  #Following 2 shits for mailer
+
+  config.action_mailer.delivery_method = :letter_opener
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
